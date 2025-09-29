2c0589261f6ce7b9cd297ec99b850eb350086947
chandruk <chandruk@localhost>
2025-09-29 15:07:08 +0530

    day 3 completed

--- corejava/src/day3/Encapulation/Encapsulation.java
+++ corejava/src/day3/Encapulation/Encapsulation.java
@@ -3,8 +3,12 @@ package day3.Encapulation;
 public class Encapsulation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		OopsConcept obj = new OopsConcept();
+		obj.setSerialNum(6);
+		obj.setName("Chandru");
+		obj.setAge(20);
+		System.out.println(obj);
+		
 	}
 
 }
