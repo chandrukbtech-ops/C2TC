27b5f78f8e69fe5a38943edb80d0633f3a68c3a5
chandruk <chandruk@localhost>
2025-10-02 08:52:16 +0530

    day 4 completed

---
+++ corejava/src/day4/ThirdPackage/PrivateConDemo.java
@@ -0,0 +1,17 @@
+package day4.ThirdPackage;
+
+public class PrivateConDemo {
+public static void main(String s[]) {
+		
+		
+		MyClass m=MyClass.getObject();
+		m.setId(10);
+		
+		MyClass m1=MyClass.getObject();
+		
+		System.out.println(m);
+		System.out.println(m1);
+		
+	}
+
+}
