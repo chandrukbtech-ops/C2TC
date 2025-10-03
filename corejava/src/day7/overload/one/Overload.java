7bcecc91c627ef28f509855be350d60330b9b607
chandruk <chandruk@localhost>
2025-10-03 14:27:44 +0530

    day 7 completed

---
+++ corejava/src/day7/overload/one/Overload.java
@@ -0,0 +1,27 @@
+package day7.overload.one;
+
+import java.awt.Point;
+
+public class Overload {
+
+	public static void main(String[] args) {
+
+		System.out.println("_____Constructor Overload_____");
+		
+		Point p = new Point();
+		System.out.println(p);
+
+		Point p1 = new Point();
+		System.out.println(p1);
+
+		Point p2 = new Point();
+		System.out.println(p2);
+
+		System.out.println("_____Method Overload_____");
+		System.out.println("Addition of two integers : " + MethodOverload.addition(10, 20));
+		System.out.println("Addition of two floating numbers: " + MethodOverload.addition(10.0f, 20.f));
+		System.out.println("Addition of one integer and one float: " + MethodOverload.addition(10, 20.0f));
+		System.out.println("Addition of one float and one integer: " + MethodOverload.addition(10.0f, 20));
+		System.out.println("Addition of two Strings: " + MethodOverload.addition("Hello ", "World"));
+	}
+}
---
+++ corejava/src/day7/override/two/coverient/CovariantOverridingImp.java
@@ -0,0 +1,13 @@
+package day7.override.two.coverient;
+
+
+public class CovariantOverridingImp {
+	public static void main(String arg[]) throws CloneNotSupportedException{
+			
+		Student student1 = new Student(1, "MBA", "Arun");
+		student1.printData();
+
+		Student student2 = (Student) student1.clone();
+		student2.printData();
+	}
+}
